4b2e5a10d068831ad2928c3cbeeafac68cc1de1e
Jorde Kang <jorde@protonmail.ch>
2021-10-25 20:52:17 +0100

    Test code is now a crap packet sniffer

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
+import code
 import tap
+import time
 from EthernetFrame import EthernetFrame
+
 
 tun = tap.make_tun("tap0")
 pkts = tap.read_pkts(tun, 10)
-print("packets read:")
-for pkt in pkts:
-    print(EthernetFrame(pkt))
+print("Frames read:")
+tap.watch_tun(tun)
+code.interact(local=locals())    
